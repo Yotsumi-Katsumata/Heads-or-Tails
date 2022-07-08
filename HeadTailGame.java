e2b31586a3e54bcda886ff2dd1a760e82a1d8834
Yotsumi-Katsumata <s1290090@u-aizu.ac.jp>
2022-07-08 11:27:50 +0900

    HoT: initial revision

---
+++ HeadTailGame.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class HeadTailGame{
+    public static void main(String args[]){
+	int Heads=0, Tails=0;
+	System.out.println("Tossing a coin...");
+	Random rand=new Random();
+
+	for(int i=0;i<3;i++){
+	    System.out.print("Round "+i+": ");
+	    boolean coin=rand.nextBoolean();
+	    if(coin==true){
+		System.out.println("Heads");
+		Heads++;
+	    }
+	    else{
+		System.out.println("Tails");
+		Tails++;
+	    }
+	}
+	System.out.println("Heads: "+Heads+", Tails: "+Tails);
+    }
+} 
